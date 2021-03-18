1d4e5ea173a2cc48f59e543544cf0313d5a68f37
Cesar <mail.mansilla@gmail.com>
2021-03-17 22:11:52 -0300

    Indices dinámicos

--- index.php
+++ index.php
@@ -6,19 +6,25 @@ use Libs\BlockChain\BlockChain;
 
 $cmCoin = new BlockChain();
 
-echo "Minando bloque 1..." . PHP_EOL;
-$cmCoin->push(new Block(1, strtotime("now"), [
+$lastBlock = $cmCoin->getLastBlock();
+$nextIndex = $lastBlock->index + 1;
+echo "Minando bloque {$nextIndex}..." . PHP_EOL;
+$cmCoin->push(new Block($nextIndex, strtotime("now"), [
   'from' => '112233',
   'to' => '332211',
   'ammount' => 666
 ]));
 
-echo "Minando bloque 2..." . PHP_EOL;
-$cmCoin->push(new Block(2, strtotime("now"), [
+$lastBlock = $cmCoin->getLastBlock();
+$nextIndex = $lastBlock->index + 1;
+echo "Minando bloque {$nextIndex}..." . PHP_EOL;
+$lastBlock = $cmCoin->getLastBlock();
+$cmCoin->push(new Block($nextIndex, strtotime("now"), [
   'from' => '223344',
   'to' => '554477',
   'ammount' => 321
 ]));
 
+echo "BlockChain: " . PHP_EOL;
 echo json_encode($cmCoin, JSON_PRETTY_PRINT);
 ?>
